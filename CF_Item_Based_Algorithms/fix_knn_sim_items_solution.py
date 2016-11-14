709a3d680e45904d87194be18cf8243e78e4f4a5
Mattia92 <mattia.crippa.brqw@alice.it>
2016-11-14 23:18:53 +0100

    fix solution adding values

--- CF_Item_Based_Algorithms/fix_knn_sim_items_solution.py
+++ CF_Item_Based_Algorithms/fix_knn_sim_items_solution.py
@@ -9,17 +9,14 @@ result_users = result.drop('recommended_items', 1)
 target_users = pd.read_table('../DataSet/target_usersClean.csv', usecols=[0], names=['user_id'])
 
 addElements = pd.read_csv('../Results/submit_0_01072.csv', sep=',')
-print result
 
 for index,row in addElements.iterrows():
-    if not(row[0] in result['user_id']):
+    if not(row[0] in result['user_id'].values):
         result = result.append(row)
 result = result.sort_values(by='user_id')
-print result
-    #line = result.get_value(idx, 'user_id')
 
-# line = result.get_value(1, 'recommended_items')
-# print(line)
-# # Split the element of the string into a list
-# line = line.split()
-# print (len(line))
+for index,row in result.iterrows():
+    line = row[1]
+    # Split the element of the string into a list
+    line = line.split()
+    print (len(line))
