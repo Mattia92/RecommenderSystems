3e06a84c8634793623b590e7ec0c48e2ce9d1181
Mattia92 <mattia.crippa.brqw@alice.it>
2016-11-30 20:34:14 +0100

    MAP@5 updates

--- ValidationAlgorithm.py
+++ ValidationAlgorithm.py
@@ -75,3 +75,31 @@ def mapk(actual, predicted, k=5):
             The mean average precision at k over the input lists
     """
     return np.mean([apk(a,p,k) for a,p in zip(actual, predicted)])
+
+
+def MeanAveragePrecision(valid_filename, attempt_filename, at=10):
+    at = int(at)
+    valid = dict()
+    for line in csv.DictReader(open(valid_filename,'r')):
+        valid.setdefault(line['source_node'],set()).update(line['destination_nodes'].split(" "))
+    attempt = list()
+    for line in csv.DictReader(open(attempt_filename,'r')):
+        attempt.append([line['source_node'], line['destination_nodes'].split(" ")])
+    average_precisions = list()
+    for entry in attempt:
+        node = entry[0]
+        predictions = entry[1]
+        correct = list(valid.get(node,dict()))
+        total_correct = len(correct)
+        if len(predictions) == 0 or total_correct == 0:
+            average_precisions.append(0)
+            continue
+        running_correct_count = 0
+        running_score = 0
+        for i in range(min(len(predictions),at)):
+            if predictions[i] in correct:
+                correct.remove(predictions[i])
+                running_correct_count += 1
+                running_score += float(running_correct_count) / (i+1)
+        average_precisions.append(running_score / min(total_correct, at))
+    return sum(average_precisions) / len(average_precisions)
