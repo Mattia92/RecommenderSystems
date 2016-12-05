b9193bce597cf180ea9a6ec5c38c4d8706d34a5c
iachille <travas@hotmail.it>
2016-12-05 15:18:41 +0100

    item content matrix

--- CBAlgorithms.py
+++ CBAlgorithms.py
@@ -1,5 +1,5 @@
 # Function to build the User-User Similarity Dictionary
-def CBUserUserSimilarity(user_items_dictionary, item_users_dictionary, similarity_shrink):
+def CBUserUserSimilarity(user_attributes_dictionary, attributes_users_dictionary, similarity_shrink):
     # Create the dictionary for the user_user similarity
     # dict {user -> (list of {user -> similarity})}
     user_user_similarity_dictionary = {}
--- Recommender.py
+++ Recommender.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import CFAlgorithms
 import CBAlgorithms
+import math
+import numpy
 
 # Importing all the files needed
 cols = ['user_id', 'item_id', 'interaction', 'create_at']
@@ -9,6 +11,10 @@ interactions = pd.read_csv('DataSet/interactions.csv', sep='\t', names=cols, hea
 # This is done because in dictionary when duplicate keys encountered during assignment, the last assignment wins
 interactions = interactions.sort_values(by='create_at')
 interactions = interactions.drop('create_at', axis=1)
+
+user_cols = ['user', 'job', 'career', 'discipline', 'industry', 'country', 'region', 'experience', 'exp_years', 'exp_years_current',
+                   'edu_deg', 'edu_fiel']
+user_profile = pd.read_csv('DataSet/user_profile.csv', sep='\t',names=user_cols, header=0)
 
 items = pd.read_csv('DataSet/item_profile.csv', sep='\t', header=0)
 active_items = items[(items.active_during_test == 1)]
@@ -67,6 +73,89 @@ for user, item, interaction in interactions.values:
 # dict {item -> (list of {user -> interaction})}
 for user, item, interaction in interactions.values:
     CF_item_users_dictionary.setdefault(item, {})[user] = 1 #int(interaction)
+
+print("Create dictionaries for content based algorithm")
+# Create the dictionary needed to compute the similarity between users
+# It is the User content matrix build with dictionaries
+# Dictionary is a list of elements, each element is defined as following
+# dict {user -> (list of {attribute -> 1})}
+users_attributes = {}
+#for each row of the user_profile csv
+for i, row in user_profile.iterrows():
+    #initialize the dictionary of the user
+    users_attributes[row['user']] = {}
+    #for each attribute of the user
+    for att in user_cols:
+        if not(att == 'user'):
+             #if the attribute is jobroles then split the string obtaining the various jobs and insert them in the dictionary
+             #if the value of the field jobroles is 0 insert nothing
+             if (att == 'job'):
+                 if not(row[att] == '0'):
+                    jobs = str(row[att]).split(",")
+                    for j in jobs:
+                        users_attributes[row['user']][att + '_' + str(j)] = 1
+             # if the attribute is edu_fieldofstudies then split the string obtaining the various fields and insert them in
+             # the dictionary
+             elif(att == 'edu_fiel'):
+                 if type(row[att]) == str:
+                     fields = str(row[att]).split(",")
+                     for f in fields:
+                         users_attributes[row['user']][att + '_' + str(f)] = 1
+             # if the attribute is country dont't consider float values
+             elif(att == 'country'):
+                 if type(row[att]) == str:
+                     users_attributes[row['user']][att + '_' + str(row[att])] = 1
+             #if the column type is int or float discard Null values
+             elif(user_profile[att].dtype == numpy.int64 or user_profile[att].dtype == numpy.float64):
+                 if not(math.isnan(row[att])):
+                     users_attributes[row['user']][att + '_' + str(row[att])] = 1
+             else:
+                 users_attributes[row['user']][att + '_' + str(row[att])] = 1
+
+# Create the dictionary containing for each attribute the list of users which have it
+# Dictionary is a list of elements, each element is defined as following
+# dict {attribute -> (list of {user -> 1})}
+attributes_users = {}
+#for each row of the user_profile csv
+for i, row in user_profile.iterrows():
+    #for each attribute of the user
+    for att in user_cols:
+        if not(att == 'user'):
+             #if the attribute is jobroles then split the string obtaining the various jobs and insert them in the dictionary
+             #if the value of the field jobroles is 0 insert nothing
+             if (att == 'job'):
+                 if not(row[att] == '0'):
+                    jobs = str(row[att]).split(",")
+                    for j in jobs:
+                        # if the dictionary is not already inizialized do it
+                        if not attributes_users.has_key(att + '_' + str(j)):
+                            attributes_users[att + '_' + str(j)] = {}
+                        attributes_users[att + '_' + str(j)][row['user']] = 1
+             # if the attribute is edu_fieldofstudies then split the string obtaining the various fields and insert them in
+             # the dictionary
+             elif(att == 'edu_fiel'):
+                 if type(row[att]) == str:
+                     fields = str(row[att]).split(",")
+                     for f in fields:
+                         if not attributes_users.has_key(att + '_' + str(f)):
+                             attributes_users[att + '_' + str(f)] = {}
+                         attributes_users[att + '_' + str(f)][row['user']] = 1
+             # if the attribute is country dont't consider float values
+             elif(att == 'country'):
+                 if type(row[att]) == str:
+                     if not attributes_users.has_key(att + '_' + str(row[att])):
+                         attributes_users[att + '_' + str(row[att])] = {}
+                     attributes_users[att + '_' + str(row[att])][row['user']] = 1
+             #if the column type is int or float discard Null values
+             elif(user_profile[att].dtype == numpy.int64 or user_profile[att].dtype == numpy.float64):
+                 if not(math.isnan(row[att])):
+                     if not attributes_users.has_key(att + '_' + str(row[att])):
+                         attributes_users[att + '_' + str(row[att])] = {}
+                     attributes_users[att + '_' + str(row[att])][row['user']] = 1
+             else:
+                 if not attributes_users.has_key(att + '_' + str(row[att])):
+                     attributes_users[att + '_' + str(row[att])] = {}
+                 attributes_users[att + '_' + str(row[att])][row['user']] = 1
 
 # Compute the User-User Similarity for Collaborative Filtering User Based
 CF_user_user_similarity_dictionary = CFAlgorithms.CFUserUserSimilarity(CF_user_items_dictionary, CF_item_users_dictionary,
