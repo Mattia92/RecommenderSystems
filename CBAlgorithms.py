c38237db85639abe3242d32cb6fec922cbac9d73
iachille <travas@hotmail.it>
2016-12-06 21:04:32 +0100

    CB: user user similarity

--- CBAlgorithms.py
+++ CBAlgorithms.py
@@ -1,9 +1,59 @@
+from __future__ import division
+import math
+import numpy as np
+
 # Function to build the User-User Similarity Dictionary
-def CBUserUserSimilarity(user_attributes_dictionary, attributes_users_dictionary, similarity_shrink):
+def CBUserUserSimilarity(target_users, user_attributes_dictionary, attributes_users_dictionary, similarity_shrink):
     # Create the dictionary for the user_user similarity
     # dict {user -> (list of {user -> similarity})}
     user_user_similarity_dictionary = {}
     user_user_similarity_dictionary_num = {}
     user_user_similarity_dictionary_den1 = {}
     user_user_similarity_dictionary_den2 = {}
+    user_similarity_dictionary_norm = {}
+
     print ("Create dictionaries for CB user-user similarity")
+    # For each user in the dictionary
+    for user in user_attributes_dictionary:
+        # Calculate the similarity only for the target users
+        if user in target_users['user_id'].unique():
+            user_att = user_attributes_dictionary[user] #dictionary of all the attributes of the user
+            user_user_similarity_dictionary_num[user] = {}
+            # For eachattribute of the user
+            for att in user_att:
+                user_list = attributes_users_dictionary[att].keys() #list of users that has this attribute
+                # for first 10 users
+                for u in user_list[:10]:
+                    # Don't consider the similarity between the same users
+                    if u == user:
+                        continue
+                    else:
+                        # Create the dictionary containing the numerator of the similarity
+                        if(user_user_similarity_dictionary_num[user].has_key(u)):
+                            user_user_similarity_dictionary_num[user][u] += user_attributes_dictionary[user][att] *\
+                                                                           user_attributes_dictionary[u][att]
+                        else:
+                            user_user_similarity_dictionary_num[user][u] = user_attributes_dictionary[user][att] *\
+                                                                           user_attributes_dictionary[u][att]
+    # For each user in the dictionary
+    for user in user_attributes_dictionary:
+        # For each attribute of the user
+        for attribute in user_attributes_dictionary[user]:
+            # Calculate the norm of the vector corresponding to the user attributes
+            if (user_similarity_dictionary_norm.has_key(user)):
+                user_similarity_dictionary_norm[user] += math.exp(user_attributes_dictionary[user][attribute])
+            else:
+                user_similarity_dictionary_norm[user] = math.exp(user_attributes_dictionary[user][attribute])
+        user_similarity_dictionary_norm[user] = math.sqrt(user_similarity_dictionary_norm[user])
+
+    print ("Similarities estimate:")
+    # For each user in the dictionary
+    for user in user_user_similarity_dictionary_num:
+        user_user_similarity_dictionary[user] = {}
+        # Calculate the user-user similarity
+        for user_j in user_user_similarity_dictionary_num[user]:
+            user_user_similarity_dictionary[user][user_j] = user_user_similarity_dictionary_num[user][user_j] / \
+                                                            (user_similarity_dictionary_norm[user] * \
+                                                             user_similarity_dictionary_norm[user_j] + similarity_shrink)
+
+    return user_user_similarity_dictionary
--- test.py
+++ test.py
@@ -1,24 +1,22 @@
+from __future__ import division
 import pandas as pd
 import math
 import numpy
 import CBAlgorithms
+from collections import OrderedDict
 
 CB_UU_similarity_shrink = 10
 
 target_users = pd.read_csv('DataSet/target_users.csv')
-for i, row in target_users.iterrows():
-    if row['user_id'] == 2613732:
-        print "ciao"
-print 2613732 in target_users['user_id']
 
 user_cols = ['user', 'job', 'career', 'discipline', 'industry', 'country', 'region', 'experience', 'exp_years', 'exp_years_current',
-                   'edu_deg', 'edu_fiel']
-user_profile = pd.read_csv('DataSet/user_profile.csv', sep='\t',names=user_cols, header=0)
+             'edu_deg', 'edu_fiel']
+user_profile = pd.read_csv('DataSet/user_profile.csv', sep='\t', names=user_cols, header=0)
 
 # Create the dictionary needed to compute the similarity between users
-# It is the User content matric build with dictionaries
+# It is the User content matrix build with dictionaries
 # Dictionary is a list of elements, each element is defined as following
-# dict {user -> (list of {attribute -> 1})}
+# dict {user -> (list of {attribute -> value})}
 users_attributes = {}
 #for each row of the user_profile csv
 for i, row in user_profile.iterrows():
@@ -34,6 +32,10 @@ for i, row in user_profile.iterrows():
                     jobs = str(row[att]).split(",")
                     for j in jobs:
                         users_attributes[row['user']][att + '_' + str(j)] = 1
+            # if the aatribute is one of the following consider them only if they are not equal to 0
+             elif(att == 'career' or att == 'exp_years' or att == 'exp_years_current'):
+                 if not(row[att] == 0):
+                     users_attributes[row['user']][att + '_' + str(row[att])] = 1
              # if the attribute is edu_fieldofstudies then split the string obtaining the various fields and insert them in
              # the dictionary
              elif(att == 'edu_fiel'):
@@ -45,6 +47,14 @@ for i, row in user_profile.iterrows():
              elif(att == 'country'):
                  if type(row[att]) == str:
                      users_attributes[row['user']][att + '_' + str(row[att])] = 1
+             # only the user user having country equal to de has this attribute
+             elif(att == 'region'):
+                 if (row['country'] == 'de'):
+                     users_attributes[row['user']][att + '_' + str(row[att])] = 1
+             # if the attribute is edu_deg don't consider null value or 0
+             elif(att == 'edu_deg'):
+                 if not (math.isnan(row[att]) or row[att] == 0):
+                     users_attributes[row['user']][att + '_' + str(row[att])] = 1
              #if the column type is int or float discard Null values
              elif(user_profile[att].dtype == numpy.int64 or user_profile[att].dtype == numpy.float64):
                  if not(math.isnan(row[att])):
@@ -54,7 +64,7 @@ for i, row in user_profile.iterrows():
 
 # Create the dictionary containing for each attribute the list of users which have it
 # Dictionary is a list of elements, each element is defined as following
-# dict {attribute -> (list of {user -> 1})}
+# dict {attribute -> (list of {user -> value})}
 attributes_users = {}
 #for each row of the user_profile csv
 for i, row in user_profile.iterrows():
@@ -86,6 +96,24 @@ for i, row in user_profile.iterrows():
                      if not attributes_users.has_key(att + '_' + str(row[att])):
                          attributes_users[att + '_' + str(row[att])] = {}
                      attributes_users[att + '_' + str(row[att])][row['user']] = 1
+             # only the user user having country equal to de has this attribute
+             elif (att == 'region'):
+                 if (row['country'] == 'de'):
+                     if not attributes_users.has_key(att + '_' + str(row[att])):
+                         attributes_users[att + '_' + str(row[att])] = {}
+                     attributes_users[att + '_' + str(row[att])][row['user']] = 1
+            # if the aatribute is one of the following consider them only if they are not equal to 0
+             elif(att == 'career' or att == 'exp_years' or att == 'exp_years_current'):
+                 if not (row[att] == 0):
+                     if not attributes_users.has_key(att + '_' + str(row[att])):
+                         attributes_users[att + '_' + str(row[att])] = {}
+                     attributes_users[att + '_' + str(row[att])][row['user']] = 1
+             # if the attribute is edu_deg don't consider null value or 0
+             elif(att == 'edu_deg'):
+                 if not (math.isnan(row[att]) or row[att] == 0):
+                     if not attributes_users.has_key(att + '_' + str(row[att])):
+                         attributes_users[att + '_' + str(row[att])] = {}
+                     attributes_users[att + '_' + str(row[att])][row['user']] = 1
              #if the column type is int or float discard Null values
              elif(user_profile[att].dtype == numpy.int64 or user_profile[att].dtype == numpy.float64):
                  if not(math.isnan(row[att])):
@@ -97,5 +125,26 @@ for i, row in user_profile.iterrows():
                      attributes_users[att + '_' + str(row[att])] = {}
                  attributes_users[att + '_' + str(row[att])][row['user']] = 1
 
+# create the tf(time frequency) dictionary for each user
+# each attribute of the same user has the same tf value
+users_tf = {}
+for user in users_attributes.keys():
+    users_tf[user] = 1 / len(users_attributes[user])
+# create the idf dictionary for each attribute
+attributes_idf = {}
+n_users = len(users_attributes.keys())
+for attribute in attributes_users.keys():
+    attributes_idf[attribute] = math.log10(n_users /len(attributes_users[attribute]))
+# modify each attribute value including tf-idf
+for user in users_attributes.keys():
+    for attribute in users_attributes[user].keys():
+        users_attributes[user][attribute] *= users_tf[user]*attributes_idf[attribute]
+        attributes_users[attribute][user] *= users_tf[user]*attributes_idf[attribute]
+# sort the dictionary by attribute values
+for attribute in attributes_users.keys():
+    attributes_users[attribute] = OrderedDict(
+        sorted(attributes_users[attribute].items(), key=lambda t: -t[1]))
+
+# user-user similarity dictionary
 CB_user_user_similarity_dictionary = CBAlgorithms.CBUserUserSimilarity(target_users, users_attributes, attributes_users, CB_UU_similarity_shrink)
 
