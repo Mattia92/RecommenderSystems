42426db13c087d2b3a222437e0cb1c143ba8912c
Mattia92 <mattia.crippa.brqw@alice.it>
2017-01-11 16:05:04 +0100

    Update Recommender CFs

--- Recommender/CF_item_based.py
+++ Recommender/CF_item_based.py
@@ -14,6 +14,10 @@ items = pd.read_csv('../DataSet/item_profile.csv', sep='\t', header=0)
 active_items = items[(items.active_during_test == 1)]
 active_items_idx = active_items[['item_id', 'active_during_test']]
 
+item_cols = ['item', 'title', 'career',	'discipline', 'industry', 'country', 'region', 'latitude', 'longitude',
+            'employ', 'tags', 'created_at', 'active_during_test']
+item_profile = pd.read_csv('DataSet/item_profile.csv', sep='\t', names=item_cols, header=0)
+
 target_users = pd.read_csv('../DataSet/target_users.csv')
 
 # Dictionary with only active items
@@ -27,6 +31,12 @@ CF_IB_predictions_output = "../Predictions/CF_Item_Based.csv"
 # Shrink values for Collaborative Filtering Item Based
 CF_IB_similarity_shrink = 20
 CF_IB_prediction_shrink = 10
+
+# Shrink values for Content Item Based
+CB_IB_similarity_shrink = 5
+
+# Weight for Hybrid Collaborative Filtering
+CF_HB_IB_w = 1.5
 
 # Values of KNN for CF Similarities, KNN = 0 means to not use the KNN technique
 CF_IB_KNN = 0
@@ -50,9 +60,17 @@ for user, item, interaction in interactions.values:
 for user, item, interaction in interactions.values:
     CF_item_users_dictionary.setdefault(item, {})[user] = 1 #int(interaction)
 
+# Dictionaries for Content Item Based Algorithms
+CB_item_attributes_dictionary, CB_attribute_items_dictionary = CBAlgorithms.InitializeDictionaries_item(item_profile, item_cols)
+
+# Compute TF and IDF
+print ("Computing TF and IDF")
+CB_item_attributes_dictionary, CB_attribute_items_dictionary = CBAlgorithms.ComputeTF_IDF(CB_item_attributes_dictionary, CB_attribute_items_dictionary)
+
 # Compute the Item-Item Similarity for Collaborative Filtering Item Based
-CF_item_item_similarity_dictionary = CFAlgorithms.CFItemItemSimilarity(CF_user_items_dictionary, CF_item_users_dictionary,
-                                                                        CF_IB_similarity_shrink, CF_IB_KNN)
+CF_item_item_similarity_dictionary = CFAlgorithms.CFHybridItemItemSimilarity(CF_user_items_dictionary, CF_item_users_dictionary,
+                                                                                CB_item_attributes_dictionary, CF_IB_similarity_shrink,
+                                                                                CB_IB_similarity_shrink, CF_IB_KNN, CF_HB_IB_w)
 
 # Compute the Prediction for Collaborative Filtering Item Based
 CF_IB_users_prediction_dictionary = CFAlgorithms.CFItemBasedPredictNormalizedRecommendation(target_users, CF_item_item_similarity_dictionary,
--- Recommender/CF_user_based.py
+++ Recommender/CF_user_based.py
@@ -14,6 +14,10 @@ items = pd.read_csv('../DataSet/item_profile.csv', sep='\t', header=0)
 active_items = items[(items.active_during_test == 1)]
 active_items_idx = active_items[['item_id', 'active_during_test']]
 
+user_cols = ['user', 'job', 'career', 'discipline', 'industry', 'country', 'region', 'experience', 'exp_years', 'exp_years_current',
+             'edu_deg', 'edu_fiel']
+user_profile = pd.read_csv('DataSet/user_profile.csv', sep='\t',names=user_cols, header=0)
+
 target_users = pd.read_csv('../DataSet/target_users.csv')
 
 # Dictionary with only active items
@@ -27,6 +31,12 @@ CF_UB_predictions_output = "../Predictions/CF_User_Based.csv"
 # Shrink values for Collaborative Filtering User Based
 CF_UB_similarity_shrink = 10
 CF_UB_prediction_shrink = 10
+
+# Shrink values for Content User Based
+CB_UB_similarity_shrink = 10
+
+# Weight for Hybrid Collaborative Filtering
+CF_HB_UB_w = 1.1
 
 # Values of KNN for CF Similarities, KNN = 0 means to not use the KNN technique
 CF_UB_KNN = 130
@@ -52,10 +62,17 @@ for user, item, interaction in interactions.values:
 for user, item, interaction in interactions.values:
     CF_item_users_dictionary.setdefault(item, {})[user] = 1 #int(interaction)
 
+# Dictionaries for Content User Based Algorithms
+CB_user_attributes_dictionary, CB_attribute_users_dictionary = CBAlgorithms.InitializeDictionaries_user(user_profile, user_cols)
+
+# Compute TF and IDF
+print ("Computing TF and IDF")
+CB_user_attributes_dictionary, CB_attribute_users_dictionary = CBAlgorithms.ComputeTF_IDF(CB_user_attributes_dictionary, CB_attribute_users_dictionary)
 
 # Compute the User-User Similarity for Collaborative Filtering User Based
-CF_user_user_similarity_dictionary = CFAlgorithms.CFUserUserSimilarity(CF_user_items_dictionary, CF_item_users_dictionary,
-                                                                        CF_UB_similarity_shrink, CF_UB_KNN)
+CF_user_user_similarity_dictionary = CFAlgorithms.CFHybridUserUserSimilarity(CF_user_items_dictionary, CF_item_users_dictionary,
+                                                                                CB_user_attributes_dictionary, CF_UB_similarity_shrink,
+                                                                                CB_UB_similarity_shrink, CF_UB_KNN, CF_HB_UB_w)
 
 # Compute the Prediction for Collaborative Filtering User Based
 CF_UB_users_prediction_dictionary = CFAlgorithms.CFUserBasedPredictNormalizedRecommendation(target_users, CF_user_user_similarity_dictionary,
